74b18deb2aa6c4ca82555c21aa403641e969ce14
Nikita Hohlov <nikita.hohlov@cauri.com>
2021-10-12 13:35:18 +0300

    CB-2/feat/ сделал репозиторий для получения данных из модели

---
+++ common/bag/BagRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\common\bag;
+
+use app\common\good\Good;
+use app\domain\bag\interfaces\Bag;
+use Yii;
+use app\domain\bag\interfaces\BagRepository as BagRepositoryInterface;
+
+class BagRepository implements BagRepositoryInterface
+{
+    private Bag $bag;
+
+    public function __construct()
+    {
+        $session = Yii::$app->session;
+        $this->bag = $session['bag'];
+    }
+
+    public function getBagModel(): Bag
+    {
+        return $this->bag;
+    }
+
+    public function checkGoodAddedToBagById(int $id): bool
+    {
+        return isset($this->bag->goods[$id]);
+    }
+
+    /* @inheritDoc */
+    public function addGood(Good $good): void
+    {
+        $this->bag->goods[$good->getId()] = $good;
+        $this->bag->goodsCount[$good->getId()] = ['count' => 1];
+    }
+
+    /* @inheritDoc */
+    public function addGoodCountById(string $id): void
+    {
+        ++$this->bag->goodsCount[$id]['count'];
+    }
+
+    /* @inheritDoc */
+    public function removeGoodById(string $id): void
+    {
+        $count = $this->bag->goodsCount[$id];
+        if ($count['count'] === 1){
+            unset($this->bag->goods[$id]);
+        }
+        --$this->bag->goodsCount[$id]['count'];
+    }
+
+    public function setPriceById(string $price, int $id): void
+    {
+        $this->bag->goods[$id]->setPrice($price);
+    }
+}
